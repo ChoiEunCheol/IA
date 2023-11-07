5a30621f345968c866a36a34accbeafecbe1d5a2
최은철 <wndqhrgkdlel@gmail.com>
2023-11-07 14:51:35 +0900

    json파일 불러오기, 햄버거 메뉴

--- frontend/index.js
+++ frontend/index.js
@@ -1,33 +1,64 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const inputField = document.getElementById('inputField');
-    const submitButton = document.getElementById('submitButton');
-    const responseDiv = document.getElementById('response');
+document.addEventListener("DOMContentLoaded", function () {
+  let userInfo; // userInfo를 전역 변수로 정의
+  let logoInfo; // logoInfo를 전역 변수로 정의
+  const inputField = document.getElementById("inputField");
+  const submitButton = document.getElementById("submitButton");
+  const responseDiv = document.getElementById("response");
+  const smileyIcon = document.getElementById("smiley");
+  const menuButton = document.getElementById("menu");
+  const menuContainer = document.querySelector(".menu-container");
 
-    submitButton.addEventListener('click', function () {
-        const inputData = inputField.value;
-
-        // AJAX 요청을 사용하여 서버에 데이터를 전송
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', '/submit-data', true);
-        xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                const response = JSON.parse(xhr.responseText);
-                responseDiv.innerHTML = `서버 응답: ${response.message}`;
-
-                // 유저 정보를 출력
-                displayUserInfo(response.userInfo);
-            }
-        };
-
-        const dataToSend = JSON.stringify({ data: inputData });
-        xhr.send(dataToSend);
-    });
-
-    // 유저 정보 출력 함수
-    function displayUserInfo(userInfo) {
-        const userInfoDiv = document.getElementById('userInfo');
-        userInfoDiv.innerHTML = `이름: ${userInfo.name}, 상태: ${userInfo.status}, 아바타: ${userInfo.avatar}`;
+  menuButton.addEventListener("click", function () {
+    // 메뉴 버튼을 클릭했을 때 메뉴가 나타나도록 토글
+    if (menuContainer.style.display === "block") {
+      menuContainer.style.display = "none";
+    } else {
+      menuContainer.style.display = "block";
     }
+  });
+  submitButton.addEventListener("click", function () {
+    const inputData = inputField.value;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "/submit-data", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        responseDiv.innerHTML = `서버 응답: ${response.message}`;
+
+        // userInfo와 logoInfo를 전역 변수에 할당
+        userInfo = response.userInfo;
+        logoInfo = response.logoInfo;
+
+        displayUserInfo(userInfo);
+
+        // 서버로부터 전송된 logoInfo 사용
+        if (logoInfo) {
+          setLogo(logoInfo);
+        }
+      }
+    };
+
+    const dataToSend = JSON.stringify({ data: inputData });
+    xhr.send(dataToSend);
+  });
+
+  function displayUserInfo(userInfo) {
+    const userInfoDiv = document.getElementById("userInfo");
+    userInfoDiv.innerHTML = `이름: ${userInfo.name}, 상태: ${userInfo.status}, 아바타: ${userInfo.avatar}`;
+  }
+
+  // logoInfo를 설정하는 함수
+  function setLogo(logoInfo) {
+    // smileyIcon을 로고로 설정
+    smileyIcon.textContent = logoInfo;
+  }
+
+  const h1 = document.getElementsByTagName("h1");
+  h1[0].addEventListener("click", () => {
+    // 이제 userInfo와 logoInfo를 사용할 수 있습니다.
+    console.log(userInfo, logoInfo);
+  });
 });
--- server/routes.js
+++ server/routes.js
@@ -1,32 +1,47 @@
-const express = require("express");
-const path = require("path");
+// routes.js
+const express = require('express');
+const path = require('path');
 const router = express.Router();
-const fs = require("fs"); // 파일 시스템 모듈을 가져옵니다.
+const fs = require('fs');
 
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
-router.post("/submit-data", (req, res) => {
+router.post('/submit-data', (req, res) => {
   const inputData = req.body.data;
-  // 이전 코드에서 데이터 처리 로직을 작성합니다.
 
-  // 사용자 정보를 읽어와 클라이언트로 전송
-  const userInfo = readUserInfo(); // 사용자 정보를 읽어오는 함수
+  // 사용자 정보 및 로고 정보를 읽어와 클라이언트로 전송
+  const userInfo = readUserInfo();
+  console.log(userInfo);
 
-  res.json({ message: `서버에서 받은 데이터: ${inputData}, ${userInfo.name}`});
+  const logoInfo = readLogoInfo();
+  console.log(logoInfo);
+
+
+  res.json({ message: `서버에서 받은 데이터: ${inputData}`, userInfo, logoInfo }); // logoInfo를 서버 응답에 추가
 });
 
 function readUserInfo() {
   try {
-    // data.json 파일을 읽어 사용자 정보를 반환
-    const dataPath = path.join(__dirname, "data.json");
-    const data = fs.readFileSync(dataPath, "utf8");
+    const dataPath = path.join(__dirname, 'data.json');
+    const data = fs.readFileSync(dataPath, 'utf8');
     const jsonData = JSON.parse(data);
-    console.log(jsonData.mainContent.userInfo);
     return jsonData.mainContent.userInfo;
   } catch (error) {
-    console.error("사용자 정보를 읽어오는 동안 오류 발생:", error);
+    console.error('사용자 정보를 읽어오는 동안 오류 발생:', error);
+    return {};
+  }
+}
+
+function readLogoInfo() {
+  try {
+    const dataPath = path.join(__dirname, 'data.json');
+    const data = fs.readFileSync(dataPath, 'utf8');
+    const jsonData = JSON.parse(data);
+    return jsonData.header.logo;
+  } catch (error) {
+    console.error('로고 정보를 읽어오는 동안 오류 발생:', error);
     return {};
   }
 }
