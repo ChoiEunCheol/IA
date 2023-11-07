23089b0a679da9aaa67f0d99e5d3f84bdd7589de
최은철 <wndqhrgkdlel@gmail.com>
2023-11-07 16:10:54 +0900

    브랜치 바꿔서 실험하기 전 커밋

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const fs = require('fs');
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 router.post('/submit-data', (req, res) => {
@@ -24,6 +24,19 @@ router.post('/submit-data', (req, res) => {
 
 const dataPath = path.join(__dirname, 'data.json');
 
+router.get('/get-data', (req, res) => {
+    try {
+      const dataPath = path.join(__dirname, 'data.json');
+      const data = fs.readFileSync(dataPath, 'utf8');
+      const jsonData = JSON.parse(data);
+      
+      // 클라이언트에게 JSON 데이터 응답
+      res.json(jsonData);
+    } catch (error) {
+      console.error('데이터를 읽어오는 동안 오류 발생:', error);
+      res.status(500).json({ error: '데이터를 불러오지 못했습니다.' });
+    }
+  });
 // router.get('/', (req, res) => {
 //   try {
 //     const data = fs.readFileSync(dataPath, 'utf8');
