704a6c2d7e101d9798a921e9d17984723a6a647f
최은철 <wndqhrgkdlel@gmail.com>
2023-11-07 16:14:20 +0900

    파일 경로 수정

--- server/app.js
+++ server/app.js
@@ -10,11 +10,11 @@ app.use(express.json());
 app.use('/', routes); // 라우터를 애플리케이션에 연결
 
 // 정적 파일을 서비스하기 위해 express.static 미들웨어를 사용합니다.
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
   // index.html 파일을 루트 경로('/')에서 반환합니다.
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(PORT, () => {
