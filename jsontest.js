1629143c8724d6aea25b42df414270c9110bea44
최은철 <wndqhrgkdlel@gmail.com>
2023-11-08 16:50:35 +0900

    파일 경로 수정

--- jsontest.js
+++ jsontest.js
@@ -1,31 +1,28 @@
 const fs = require('fs');
 const filePath = 'test.json';
 
-// 1. 파일 읽기
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.error('파일 읽기 오류:', err);
-    return 
+    return;
   }
 
-  // 2. JSON 데이터 파싱
+  // JSON 데이터 파싱
   const jsonData = JSON.parse(data);
-  console.log(jsonData);
-  
-  // 3. 데이터 수정
-  jsonData.userInfo.name = '새로운 이름';
-  jsonData.userInfo.status = '오프라인';
 
-  // 4. 수정된 데이터를 JSON 문자열로 변환
+  // 새로운 키와 값을 추가
+  jsonData.newKey = '제이슨';
+
+  // JSON 데이터를 다시 문자열로 변환
   const updatedData = JSON.stringify(jsonData, null, 2);
 
-  // 5. 파일 쓰기
+  // 파일 쓰기
   fs.writeFile(filePath, updatedData, (err) => {
     if (err) {
       console.error('파일 쓰기 오류:', err);
       return;
     }
 
-    console.log('파일이 업데이트되었습니다.');
+    console.log('새로운 키와 값이 JSON 파일에 추가되었습니다.');
   });
 });
