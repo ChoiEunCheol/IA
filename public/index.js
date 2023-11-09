ee7077e05e1c59e0f8fb258c3bf7be68f7161c12
최은철 <wndqhrgkdlel@gmail.com>
2023-11-09 17:02:01 +0900

    스크롤이 항상 아래에 위치하게 조절

--- public/index.js
+++ public/index.js
@@ -22,39 +22,42 @@ document.addEventListener("DOMContentLoaded", function () {
   function handleSubmit() {
     const inputData = inputField.value;
     const xhr = new XMLHttpRequest();
-  
+
     xhr.open("POST", "/submit-data", true);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  
+
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4 && xhr.status === 200) {
         const response = JSON.parse(xhr.responseText);
-  
-        const newDiv = createResponseDiv(`서버 응답: ${response.message}`);
+
+        const newDiv = createResponseDiv(`서버 응답: ${response.message}`, "response");
         responseContainer.prepend(newDiv);
-  
+
         // userInfo를 설정하고 displayUserInfo를 호출
         userInfo = response.userInfo;
         displayUserInfo(userInfo);
-  
+
         // logoInfo를 설정하고 setLogo를 호출
         logoInfo = response.logoInfo;
         setLogo(logoInfo.logo);
-  
+
         // inputRecords 업데이트
         const inputRecords = response.inputRecords;
         displayInputRecords(inputRecords);
+
+        // 서버 응답 후 스크롤을 가장 아래로 조절
+        scrollToBottom();
       }
     };
-  
+
     const dataToSend = JSON.stringify({ data: inputData });
     xhr.send(dataToSend);
   }
-  
+
   function displayInputRecords(inputRecords) {
     const inputRecordsDiv = document.getElementById("responseContainer");
     inputRecordsDiv.innerHTML = '';
-  
+
     inputRecords.forEach(record => {
       const responseDiv = createResponseDiv("","response");
       const messageDiv = createResponseDiv(`${record.message}`,"message");
@@ -80,34 +83,33 @@ document.addEventListener("DOMContentLoaded", function () {
     smileyIcon.textContent = logoInfo;
   }
 
-  function createResponseDiv(content,className) {
+  function createResponseDiv(content, className) {
     const newDiv = document.createElement('div');
     newDiv.textContent = content;
     newDiv.classList = className;
     return newDiv;
   }
 
-  // 서버로부터 전송된 logoInfo 사용
-  if (logoInfo) {
-    setLogo(logoInfo.logo);
+  // 클라이언트 측에서 모든 inputRecords를 삭제하는 함수
+  function deleteAllInputRecords() {
+    fetch('http://localhost:3000/delete-records', {
+      method: 'POST',
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.message); // 성공적인 삭제 메시지 출력
+        responseContainer.innerHTML = ''; // 삭제 후 렌더링된 메시지 초기화
+      })
+      .catch(error => {
+        console.error('Error deleting records:', error);
+      });
   }
 
-  // 클라이언트 측에서 모든 inputRecords를 삭제하는 함수
-function deleteAllInputRecords() {
-  fetch('http://localhost:3000/delete-records', {
-    method: 'POST',
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.message); // 성공적인 삭제 메시지 출력
-      responseContainer.innerHTML = ''; // 삭제 후 렌더링된 메시지 초기화
-    })
-    .catch(error => {
-      console.error('Error deleting records:', error);
-    });
-}
+  // 예를 들어, 버튼 클릭 시 모든 inputRecords 삭제 기능을 실행
+  smileyIcon.addEventListener("click", deleteAllInputRecords);
 
-// 예를 들어, 버튼 클릭 시 모든 inputRecords 삭제 기능을 실행
-smileyIcon.addEventListener("click", deleteAllInputRecords);
+  // 스크롤을 가장 아래로 조절하는 함수
+  function scrollToBottom() {
+    responseContainer.scrollTop = responseContainer.scrollHeight;
+  }
 });
-
